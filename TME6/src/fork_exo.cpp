24bd3d138020eca33f96982d92b7136437bfa9a3
21311838 <21311838@ppti-24-304-16.ufr-info-p6.jussieu.fr>
2023-10-25 17:55:15 +0200

    envoie

--- TME6/src/fork_exo.cpp
+++ TME6/src/fork_exo.cpp
@@ -1,18 +1,69 @@
 #include <iostream>
 #include <unistd.h>
+#include <sys/wait.h>
+#include "rsleep.h"
+
+
+int ptVie = 3;
+
+
+void attaque(pid_t adversaire){
+	signal(SIGINT,[](int signal){
+		if(signal == SIGINT){
+			ptVie--;
+			std::cout<< "ptVie"<< ptVie <<std::endl;
+		}
+		if(ptVie == 0){
+			std::cout<< "sortie"<<std::endl;
+			exit(1);
+		}
+	});
+	kill(adversaire,SIGINT);
+	while(true){
+		std::cout<< "fin processus"<<std::endl;
+		exit(0);
+	}
+	randsleep();
+}
+void defense(){
+	signal(SIGINT,SIG_IGN);
+	randsleep();
+
+}
+void combat(pid_t adversaire){
+	while(true){
+		defense();
+		attaque(adversaire);
+	}
+}
 
 int main () {
+	int cpt =0;
 	const int N = 3;
 	std::cout << "main pid=" << getpid() << std::endl;
-
-	for (int i=1, j=N; i<=N && j==N && fork()==0 ; i++ ) {
+	for (int i=1, j=N; i<=N && j==N  ; i++ ) {
+		if(fork()!=0){
+			cpt++;
+			break;
+		}
+		cpt =0;
 		std::cout << " i:j " << i << ":" << j << std::endl;
 		for (int k=1; k<=i && j==N ; k++) {
 			if ( fork() == 0) {
+				cpt = 0;
 				j=0;
 				std::cout << " k:j " << k << ":" << j << std::endl;
+
+			}else{
+				cpt++;
 			}
 		}
+
 	}
+	for(int i=0;i<cpt;i++){
+				int tps = wait(nullptr);
+				std::cout << "tps = " << tps << std::endl;
+
+			}
 	return 0;
 }
