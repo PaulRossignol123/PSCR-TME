4e0f2e8824b6153f4fd582e1d77df9d859df64e5
Paul <Paul@DESKTOP-E3DQJJ2>
2023-10-30 13:08:28 +0100

    Fin tme2

--- TME2/tme2/src/HashMap.h
+++ TME2/tme2/src/HashMap.h
@@ -1,68 +1,128 @@
-#include <cstddef>
-#include <vector>
-#include <forward_list>
 #include <iostream>
-using namespace std;
-namespace pr {
+#include <fstream>
+#include <regex>
+#include <chrono>
+#include <vector>
+#include "HashMap.h"
 
-template <typename K, typename V>
+template <typename Iterator>
+size_t count(Iterator begin,Iterator end){
+	size_t count = 0;
+	while(begin != end){
+		count++;
+		++begin;
+	}
+	return count;
+}
+template <typename Iterator,typename  T>
+size_t count_if_equal(Iterator begin,Iterator end, const T& val){
+		size_t count = 0;
+	while(begin != end){
+		if(*begin == val){
+			count++;
+		}
+		++begin;
+	}
+	return count;
+}
+bool sortByValueDescending(const std::pair<std::string, int>& a, const std::pair<std::string, int>& b) {
+    return a.second > b.second;
+}
 
-class HashTable{
-    public :
-    class Entry{
-        public:
-        const K key;
-        V value;
-        Entry(const K& key, const V& value):key(key), value(value){}
-    };
+int main () {
 
-    private :
-    std::vector <std::forward_list<Entry>> bucket ;
-    size_t sz;                    //Nombre d'entrées
+	using namespace std;
+	using namespace std::chrono;
+	using namespace pr;
 
-    public :
-    HashTable(size_t szb) : bucket(szb){
-        sz = 0;
-    }
+	ifstream input = ifstream("WarAndPeace.txt");
 
-    // (Les mécanismes de la classe canonique sont implémentés par défaut par le compilateur, pas besoin de les réécrire.)
+	auto start = steady_clock::now();
+	cout << "Parsing War and Peace" << endl;
+	size_t t = 20333;
+	HashTable<string,int>h(t);
+	size_t nombre_lu = 0;
+	// prochain mot lu
+	string word;
+	// une regex qui reconnait les caractères anormaux (négation des lettres)
+	std::vector<std::pair<std::string,int>> vc;
 
-    /*Fonction qui rend l'adresse de la valeur associée à la clé key si on la trouve, nullptr sinon*/
-    V* get (const K & key){
-        size_t ind = (std::hash<K>() (key)) % bucket.size();    //Hasher standard
+	regex re( R"([^a-zA-Z])");
+	while (input >> word) {
+		bool pres = false;
+		// élimine la ponctuation et les caractères spéciaux
+		word = regex_replace ( word, re, "");
+		// passe en lowercase
+		transform(word.begin(),word.end(),word.begin(),::tolower);
+		// word est maintenant "tout propre"
 
-        /*Trouver le mot dans la hashmap*/
-        for (auto& e : bucket[ind]){
-            if (e.key == key){
-                return &e.value;
-            }
-        }
-        return nullptr;
-    }
-    /*Fonction qui ajoute l'association (key, value) à la table.
-    * La fonction rend vrai si la valeur associée à key a été mise à jour dans la table,
-    * et faux si on a réalisé une insertion (la clé n'était pas encore dans la table).*/
-    bool put (const K & key, const V value){
-        size_t ind = std::hash<K>() (key) % bucket.size();
+		if(h.get(word) == nullptr){
+			h.put(word,1);
+		}else{
+			int x = *h.get(word);
+			x++;
+			h.put(word,x);
+		}
 
-        for(auto & e : bucket[ind]){
-		    if (e.key == key){
-                e.value = value;
-                return true;
-            }
-    	}
-        /*On ne l'a pas trouvé dans le bucket, il faut créer l'association dans la table*/
-        bucket[ind].emplace_front(key,value);           //emplace évite de créer un temporaire
-        ++sz;                 //La taille de la Hashtable augmente
-        return false;           //On a créé une insertion
-    }
-    /*Fonction qui indique la taille de la hashtable (nombre d'entrées effectives dedans)*/
-    size_t size() const{return sz;}
-    };
-	template<typename Iterator>
-		Iterator find(Iterator lit,Iterator end){
+		if (nombre_lu % 100 == 0)
+			// on affiche un mot "propre" sur 100
+				cout << nombre_lu << ": "<< word << endl;
+		nombre_lu++;
+		for(auto& paire:vc){
+			std::string chaine = paire.first;
+
+			if(chaine == word){
+				pres = true;
+				paire.second += 1;
+				break;
+			}
+
+		}
+		if(pres == false){
+
+			vc.push_back(std::make_pair(word,1));
+		}
 
 	}
+	input.close();
+	cout << "Finished Parsing War and Peace" << endl;
+
+	 std::vector<pair<string,int>>vector1;
+
+	 for (size_t i = 0; i < h.size(); i++) {
+	        for (const auto& entry : h.bucket[i]) {
+	            vector1.push_back(make_pair(entry.key, entry.value));
+	        }
+	    }
+	 std::sort(vector1.begin(), vector1.end(), sortByValueDescending);
+	 for (int i = 0; i < 10; i++) {
+	        std::cout << "Mot : " << vector1[i].first << ", Occurrences : " << vector1[i].second << std::endl;
+	    }
 
 
+
+
+	cout << "Nombre de mots non diff" << h.size() << endl;
+
+	auto end = steady_clock::now();
+    cout << "Parsing took "
+              << duration_cast<milliseconds>(end - start).count()
+              << "ms.\n";
+
+    cout << "Found a total of " << nombre_lu << " words." << endl;
+
+
+
+    int arr[] = {1,2,3,2,2,4,5,6};
+	int val = 2;
+	size_t result = count(arr, arr+sizeof(arr)/sizeof(arr[0]));
+	size_t result2 = count_if_equal(arr, arr+sizeof(arr)/sizeof(arr[0]),val);
+	std::cout <<"Le nombre d'éléments est :" << result << std::endl;
+	std::cout <<"Le nombre d'éléments égaux à 2 est :  " << result2 << std::endl;
+
+
+
+
+
+	return 0;
 }
--- TME2/tme2/src/main.cpp
+++ TME2/tme2/src/main.cpp
@@ -25,23 +25,26 @@ size_t count_if_equal(Iterator begin,Iterator end, const T& val){
 	}
 	return count;
 }
-
+bool sortByValueDescending(const std::pair<std::string, int>& a, const std::pair<std::string, int>& b) {
+    return a.second > b.second;
+}
 
 int main () {
 
 	using namespace std;
 	using namespace std::chrono;
 	using namespace pr;
-	/*
+
 	ifstream input = ifstream("WarAndPeace.txt");
 
 	auto start = steady_clock::now();
 	cout << "Parsing War and Peace" << endl;
-
+	size_t t = 20333;
+	HashTable<string,int>h(t);
 	size_t nombre_lu = 0;
 	// prochain mot lu
 	string word;
-	// une regex qui reconnait les cara  ctères anormaux (négation des lettres)
+	// une regex qui reconnait les caractères anormaux (négation des lettres)
 	std::vector<std::pair<std::string,int>> vc;
 
 	regex re( R"([^a-zA-Z])");
@@ -52,9 +55,19 @@ int main () {
 		// passe en lowercase
 		transform(word.begin(),word.end(),word.begin(),::tolower);
 		// word est maintenant "tout propre"
+
+		if(h.get(word) == nullptr){
+			h.put(word,1);
+		}else{
+			int x = *h.get(word);
+			x++;
+			h.put(word,x);
+		}
+
 		if (nombre_lu % 100 == 0)
 			// on affiche un mot "propre" sur 100
-			cout << nombre_lu << ": "<< word << endl;
+				cout << nombre_lu << ": "<< word << endl;
+		nombre_lu++;
 		for(auto& paire:vc){
 			std::string chaine = paire.first;
 
@@ -66,14 +79,30 @@ int main () {
 
 		}
 		if(pres == false){
-			nombre_lu++;
+
 			vc.push_back(std::make_pair(word,1));
 		}
 
 	}
 	input.close();
-
 	cout << "Finished Parsing War and Peace" << endl;
+
+	 std::vector<pair<string,int>>vector1;
+
+	 for (size_t i = 0; i < h.size(); i++) {
+	        for (const auto& entry : h.bucket[i]) {
+	            vector1.push_back(make_pair(entry.key, entry.value));
+	        }
+	    }
+	 std::sort(vector1.begin(), vector1.end(), sortByValueDescending);
+	 for (int i = 0; i < 10; i++) {
+	        std::cout << "Mot : " << vector1[i].first << ", Occurrences : " << vector1[i].second << std::endl;
+	    }
+
+
+
+
+	cout << "Nombre de mots non diff" << h.size() << endl;
 
 	auto end = steady_clock::now();
     cout << "Parsing took "
@@ -81,16 +110,19 @@ int main () {
               << "ms.\n";
 
     cout << "Found a total of " << nombre_lu << " words." << endl;
-	*/
-	int arr[] = {1,2,3,2,2,4,5,6};
+
+
+
+    int arr[] = {1,2,3,2,2,4,5,6};
 	int val = 2;
 	size_t result = count(arr, arr+sizeof(arr)/sizeof(arr[0]));
 	size_t result2 = count_if_equal(arr, arr+sizeof(arr)/sizeof(arr[0]),val);
 	std::cout <<"Le nombre d'éléments est :" << result << std::endl;
 	std::cout <<"Le nombre d'éléments égaux à 2 est :  " << result2 << std::endl;
-	size_t u = 4;
-	HashTable<int,int>*h;
+
+
+
+
+
 	return 0;
 }
-
-
